1b6b0a351188052203b6376bf070d94c927b390b
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:36 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_KantanChartsUMG_structs.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:33 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct KantanChartsUMG.CategoryStyleManualMapping
+// 0x0020
+struct FCategoryStyleManualMapping
+{
+	struct FName                                       CategoryId;                                               // 0x0000(0x0008) (Edit, ZeroConstructor, IsPlainOldData)
+	struct FKantanCategoryStyle                        Style;                                                    // 0x0008(0x0018) (Edit)
+};
+
+// ScriptStruct KantanChartsUMG.SeriesStyleManualMapping
+// 0x0028
+struct FSeriesStyleManualMapping
+{
+	struct FName                                       SeriesId;                                                 // 0x0000(0x0008) (Edit, ZeroConstructor, IsPlainOldData)
+	struct FKantanSeriesStyle                          Style;                                                    // 0x0008(0x0020) (Edit)
+};
+
+}
+
