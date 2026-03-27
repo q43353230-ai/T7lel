378957521f2373eef3735794a2bc6d69e289ccc7
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:20:57 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_LuaHotReload_parameters.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function LuaHotReload.LuaHotReloadHelper.OnLuaFileHotUpdate
+struct ULuaHotReloadHelper_OnLuaFileHotUpdate_Params
+{
+	struct FString                                     NotifyMessage;                                            // (Parm, ZeroConstructor)
+};
+
+}
+
