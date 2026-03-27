8b58b4a5855c047da6bf5b4b2760f6af5d24a46d
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:19:48 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ClusterReplication_parameters.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:33 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function ClusterReplication.ClusterReplicationSubsystem.SetAutoClearCache
+struct UClusterReplicationSubsystem_SetAutoClearCache_Params
+{
+	bool                                               Val;                                                      // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function ClusterReplication.ClusterReplicationSubsystem.SetAutoCache
+struct UClusterReplicationSubsystem_SetAutoCache_Params
+{
+	bool                                               Val;                                                      // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function ClusterReplication.ClusterReplicationSubsystem.RemoveAllCachedObjectData
+struct UClusterReplicationSubsystem_RemoveAllCachedObjectData_Params
+{
+};
+
+}
+
