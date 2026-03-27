e9a89b9738bc8aa4e1a2ab3ec5ace5364fbaf97d
q43353230-ai <q43353230@gmail.com>
2026-03-27 08:13:42 +0200

    رك

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_MoviePlayer_structs.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Enums
+//---------------------------------------------------------------------------
+
+// Enum MoviePlayer.EMoviePlaybackType
+enum class EMoviePlaybackType : uint8_t
+{
+	MT_Normal                      = 0,
+	MT_Looped                      = 1,
+	MT_LoadingLoop                 = 2,
+	MT_MAX                         = 3
+};
+
+
+
+}
+
