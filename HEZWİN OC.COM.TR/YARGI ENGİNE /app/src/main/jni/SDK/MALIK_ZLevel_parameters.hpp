cdcfb505ae559ca1b66372ee712d2d838f09b540
q43353230-ai <q43353230@gmail.com>
2026-03-27 08:14:13 +0200

    رك

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ZLevel_parameters.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function ZLevel.ZLevelData.ReBindLevelDataComponent
+struct AZLevelData_ReBindLevelDataComponent_Params
+{
+};
+
+// Function ZLevel.ZLevelData.CheckMonsterSpotIsOnLand
+struct AZLevelData_CheckMonsterSpotIsOnLand_Params
+{
+	class UZMonsterSpot*                               MonsterSpot;                                              // (Parm, ZeroConstructor, InstancedReference, IsPlainOldData)
+	class UZMonsterSpotGroup*                          SpotGroup;                                                // (Parm, ZeroConstructor, InstancedReference, IsPlainOldData)
+};
+
+}
+
