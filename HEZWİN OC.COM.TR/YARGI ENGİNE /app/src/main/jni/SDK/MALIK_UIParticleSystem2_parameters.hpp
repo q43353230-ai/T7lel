898b70a02dbeda306f2f15b13141b7ffd82bb917
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:30:21 +0200

    Smart upload - 2026-03-27 07:27:46.077176

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_UIParticleSystem2_parameters.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function UIParticleSystem2.ParticleSystemWidget2.SetParticleSystem
+struct UParticleSystemWidget2_SetParticleSystem_Params
+{
+	class UParticleSystem*                             ParticleSystem;                                           // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function UIParticleSystem2.ParticleSystemWidget2.SetNiagaraSystem
+struct UParticleSystemWidget2_SetNiagaraSystem_Params
+{
+	class UNiagaraSystem*                              ParticleSystem;                                           // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function UIParticleSystem2.ParticleSystemWidget2.SetActivate
+struct UParticleSystemWidget2_SetActivate_Params
+{
+	bool                                               bIsActivate;                                              // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function UIParticleSystem2.ParticleSystemWidget2.GetParticleComponent
+struct UParticleSystemWidget2_GetParticleComponent_Params
+{
+	class UParticleSystemComponent*                    ReturnValue;                                              // (ExportObject, Parm, OutParm, ZeroConstructor, ReturnParm, InstancedReference, IsPlainOldData)
+};
+
+// Function UIParticleSystem2.ParticleSystemWidget2.GetNiagaraComponent
+struct UParticleSystemWidget2_GetNiagaraComponent_Params
+{
+	class UNiagaraComponent*                           ReturnValue;                                              // (ExportObject, Parm, OutParm, ZeroConstructor, ReturnParm, InstancedReference, IsPlainOldData)
+};
+
+}
+
