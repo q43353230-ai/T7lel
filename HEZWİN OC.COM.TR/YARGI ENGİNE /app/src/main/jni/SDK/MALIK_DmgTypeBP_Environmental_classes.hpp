f58713f4cea23e148f516ba04cb559ec0095c29f
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:19:59 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_DmgTypeBP_Environmental_classes.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:36 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// BlueprintGeneratedClass DmgTypeBP_Environmental.DmgTypeBP_Environmental_C
+// 0x0000 (0x0030 - 0x0030)
+class UDmgTypeBP_Environmental_C : public UDamageType
+{
+public:
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("BlueprintGeneratedClass DmgTypeBP_Environmental.DmgTypeBP_Environmental_C");
+		return pStaticClass;
+	}
+
+};
+
+
+}
+
