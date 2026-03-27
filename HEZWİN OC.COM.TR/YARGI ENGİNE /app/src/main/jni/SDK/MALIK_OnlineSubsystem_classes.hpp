d21790d8b4487fcb09ed1f8f9cad3d858589cb92
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:17:11 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_OnlineSubsystem_classes.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// Class OnlineSubsystem.NamedInterfaces
+// 0x002C (0x0048 - 0x001C)
+class UNamedInterfaces : public UObject
+{
+public:
+	unsigned char                                      UnknownData00[0xC];                                       // 0x001C(0x000C) MISSED OFFSET
+	TArray<struct FNamedInterfaceDef>                  NamedInterfaceDefs;                                       // 0x0028(0x000C) (ZeroConstructor, Config)
+	unsigned char                                      UnknownData01[0x14];                                      // 0x0034(0x0014) MISSED OFFSET
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class OnlineSubsystem.NamedInterfaces");
+		return pStaticClass;
+	}
+
+};
+
+
+// Class OnlineSubsystem.TurnBasedMatchInterface
+// 0x0000 (0x0020 - 0x0020)
+class UTurnBasedMatchInterface : public UInterface
+{
+public:
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class OnlineSubsystem.TurnBasedMatchInterface");
+		return pStaticClass;
+	}
+
+
+	void OnMatchReceivedTurn(const struct FString& Match, bool bDidBecomeActive);
+	void OnMatchEnded(const struct FString& Match);
+};
+
+
+}
+
