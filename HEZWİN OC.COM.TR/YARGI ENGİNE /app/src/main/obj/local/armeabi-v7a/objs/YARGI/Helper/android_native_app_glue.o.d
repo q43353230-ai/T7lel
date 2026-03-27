88ea56be0ae5125b46174fba585a91bda624a5b6
q43353230-ai <q43353230@gmail.com>
2026-03-27 08:16:54 +0200

    رك

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/obj/local/armeabi-v7a/objs/YARGI/Helper/android_native_app_glue.o.d
@@ -0,0 +1,11 @@
+src/main/obj/local/armeabi-v7a/objs/YARGI/Helper/android_native_app_glue.o: \
+  src/main/jni/Helper/android_native_app_glue.c \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/errno.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/string.h \
+  src/main/jni/Helper/android_native_app_glue.h
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/string.h:
+
+src/main/jni/Helper/android_native_app_glue.h:
