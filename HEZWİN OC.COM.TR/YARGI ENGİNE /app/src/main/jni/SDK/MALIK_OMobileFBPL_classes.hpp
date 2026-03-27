b1190ca213ecba07a1b18ae312f6a88f3e689649
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:17:07 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_OMobileFBPL_classes.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:33 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// Class OMobileFBPL.OMobileFBPL
+// 0x0000 (0x0020 - 0x0020)
+class UOMobileFBPL : public UBlueprintFunctionLibrary
+{
+public:
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class OMobileFBPL.OMobileFBPL");
+		return pStaticClass;
+	}
+
+
+	static bool IsRunningOnBattery();
+	static bool IsBatteryStateCharging();
+	static int GetVolumeState();
+	static struct FString GetDeviceName();
+	static float GetBatteryTemperature();
+	static int GetBatteryLevel();
+	static bool AreHeadphonesPluggedIn();
+};
+
+
+}
+
