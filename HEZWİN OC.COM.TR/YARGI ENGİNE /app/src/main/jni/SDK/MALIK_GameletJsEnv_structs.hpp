6c436bb29462ccdb60086f10ea4b18ff63771597
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:04 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_GameletJsEnv_structs.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct GameletJsEnv.GameletPropertyMetaRoot
+// 0x0001
+struct FGameletPropertyMetaRoot
+{
+	unsigned char                                      UnknownData00[0x1];                                       // 0x0000(0x0001) MISSED OFFSET
+};
+
+// ScriptStruct GameletJsEnv.GameletArrayBuffer
+// 0x000C
+struct FGameletArrayBuffer
+{
+	unsigned char                                      UnknownData00[0xC];                                       // 0x0000(0x000C) MISSED OFFSET
+};
+
+// ScriptStruct GameletJsEnv.GameletJsObject
+// 0x0038
+struct FGameletJsObject
+{
+	unsigned char                                      UnknownData00[0x38];                                      // 0x0000(0x0038) MISSED OFFSET
+};
+
+}
+
