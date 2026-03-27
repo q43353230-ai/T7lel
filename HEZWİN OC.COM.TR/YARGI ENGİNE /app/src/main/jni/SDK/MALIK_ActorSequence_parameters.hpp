af77b428fa860bc3aa84c1cb1e1fa37b4e748252
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:13:12 +0200

    Ultra fast upload - 2026-03-27 07:12:30.877347

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ActorSequence_parameters.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function ActorSequence.ActorSequenceComponent.StopPlay
+struct UActorSequenceComponent_StopPlay_Params
+{
+};
+
+// Function ActorSequence.ActorSequenceComponent.StartPlay
+struct UActorSequenceComponent_StartPlay_Params
+{
+	float                                              StartTime;                                                // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function ActorSequence.ActorSequenceComponent.OnStopOrFinsh
+struct UActorSequenceComponent_OnStopOrFinsh_Params
+{
+};
+
+// Function ActorSequence.ActorSequenceComponent.GetLength
+struct UActorSequenceComponent_GetLength_Params
+{
+	float                                              ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+};
+
+}
+
