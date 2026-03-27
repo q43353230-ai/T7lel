adacbf6b4cb0bf5b2c0ee1567dcf0bda48594a76
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:48 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_MediaCompositing_parameters.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function MediaCompositing.MediaPlaneComponent.SetMediaPlane
+struct UMediaPlaneComponent_SetMediaPlane_Params
+{
+	struct FMediaPlaneParameters                       Plane;                                                    // (Parm)
+};
+
+// Function MediaCompositing.MediaPlaneComponent.OnRenderTextureChanged
+struct UMediaPlaneComponent_OnRenderTextureChanged_Params
+{
+};
+
+// Function MediaCompositing.MediaPlaneComponent.GetPlane
+struct UMediaPlaneComponent_GetPlane_Params
+{
+	struct FMediaPlaneParameters                       ReturnValue;                                              // (Parm, OutParm, ReturnParm)
+};
+
+}
+
