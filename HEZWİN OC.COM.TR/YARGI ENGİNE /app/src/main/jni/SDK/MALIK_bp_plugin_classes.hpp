291253d13ba5f0ef39af232e3b003bdbf703a39f
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:38:22 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_bp_plugin_classes.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// Class bp_plugin.bp_pluginBPLibrary
+// 0x0000 (0x0020 - 0x0020)
+class Ubp_pluginBPLibrary : public UBlueprintFunctionLibrary
+{
+public:
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class bp_plugin.bp_pluginBPLibrary");
+		return pStaticClass;
+	}
+
+
+	static void bp_pluginSendEvent(const struct FString& jsonEventCmd);
+	static int bp_pluginRequestInstallWidget(int wigetType, const struct FString& authInfo);
+	static float bp_pluginLaunchMeemoFunction(const struct FString& Param);
+	static bool bp_pluginIsInForeground();
+	static int bp_pluginGetInstalledWidgetType();
+};
+
+
+}
+
