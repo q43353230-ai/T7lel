29665353ed22c6dc7be05037226db7b32ab25f1e
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:37:29 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_Overlay_classes.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// Class Overlay.Overlays
+// 0x0004 (0x0020 - 0x001C)
+class UOverlays : public UObject
+{
+public:
+	unsigned char                                      UnknownData00[0x4];                                       // 0x001C(0x0004) MISSED OFFSET
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class Overlay.Overlays");
+		return pStaticClass;
+	}
+
+};
+
+
+// Class Overlay.BasicOverlays
+// 0x0008 (0x0028 - 0x0020)
+class UBasicOverlays : public UOverlays
+{
+public:
+	unsigned char                                      UnknownData00[0x8];                                       // 0x0020(0x0008) MISSED OFFSET
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class Overlay.BasicOverlays");
+		return pStaticClass;
+	}
+
+};
+
+
+// Class Overlay.LocalizedOverlays
+// 0x0040 (0x0060 - 0x0020)
+class ULocalizedOverlays : public UOverlays
+{
+public:
+	TMap<struct FString, class UBasicOverlays*>        LocaleToOverlaysMap;                                      // 0x0020(0x0050) (Edit, ZeroConstructor)
+	unsigned char                                      UnknownData00[0x4];                                       // 0x005C(0x0004) MISSED OFFSET
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class Overlay.LocalizedOverlays");
+		return pStaticClass;
+	}
+
+};
+
+
+}
+
