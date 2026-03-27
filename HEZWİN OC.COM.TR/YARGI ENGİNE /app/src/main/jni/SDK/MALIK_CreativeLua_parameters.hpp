86c12656bcb22ab5275fc176293115b659a2ebbc
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:19:55 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_CreativeLua_parameters.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function CreativeLua.CreativeBridgeLuaVM.UGCLuaError
+struct UCreativeBridgeLuaVM_UGCLuaError_Params
+{
+	int                                                errCode;                                                  // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function CreativeLua.CreativeBridgeLuaVM.RegisterSluaCallUgcluaEventHandler
+struct UCreativeBridgeLuaVM_RegisterSluaCallUgcluaEventHandler_Params
+{
+};
+
+// Function CreativeLua.CreativeBridgeLuaVM.PostInit
+struct UCreativeBridgeLuaVM_PostInit_Params
+{
+};
+
+}
+
