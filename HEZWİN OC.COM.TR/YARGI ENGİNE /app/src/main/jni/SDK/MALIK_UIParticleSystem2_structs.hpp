e1153fc45b0852035efc5bfa14f6862d83a5b23a
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:38:10 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_UIParticleSystem2_structs.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Enums
+//---------------------------------------------------------------------------
+
+// Enum UIParticleSystem2.UIMeshProjectionMethod
+enum class EUIMeshProjectionMethod : uint8_t
+{
+	YOZOrtho                       = 0,
+	XOYOrtho                       = 1,
+	YOZPerspectiveFullScreen       = 2,
+	YOZPerspectiveLocal            = 3,
+	UIMeshProjectionMethod_MAX     = 4
+};
+
+
+
+}
+
