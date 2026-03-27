6a0f8295c06e52de9984cf20b438420eb61897ec
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:02 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_GameRobotSync2_classes.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// Class GameRobotSync2.MyAutoTest3
+// 0x002C (0x0048 - 0x001C)
+class UMyAutoTest3 : public UObject
+{
+public:
+	unsigned char                                      UnknownData00[0x2C];                                      // 0x001C(0x002C) MISSED OFFSET
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class GameRobotSync2.MyAutoTest3");
+		return pStaticClass;
+	}
+
+};
+
+
+}
+
