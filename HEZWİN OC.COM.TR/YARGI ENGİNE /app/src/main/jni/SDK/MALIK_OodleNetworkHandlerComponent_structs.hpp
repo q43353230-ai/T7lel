8484cb81fbe430318cdb9a6141baf22b84bbc18a
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:37:28 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_OodleNetworkHandlerComponent_structs.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Enums
+//---------------------------------------------------------------------------
+
+// Enum OodleNetworkHandlerComponent.EOodleNetworkEnableMode
+enum class EOodleNetworkEnableMode : uint8_t
+{
+	EOodleNetworkEnableMode__AlwaysEnabled = 0,
+	EOodleNetworkEnableMode__WhenCompressedPacketReceived = 1,
+	EOodleNetworkEnableMode__EOodleNetworkEnableMode_MAX = 2
+};
+
+
+
+}
+
