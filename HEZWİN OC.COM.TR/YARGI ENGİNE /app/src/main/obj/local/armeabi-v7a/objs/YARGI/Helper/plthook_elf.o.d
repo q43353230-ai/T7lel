bc6583f6f5c65ee987a309260cbab2083bea3f1c
q43353230-ai <q43353230@gmail.com>
2026-03-27 08:30:43 +0200

    Smart upload - 2026-03-27 08:26:19.696989

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/obj/local/armeabi-v7a/objs/YARGI/Helper/plthook_elf.o.d
@@ -0,0 +1,25 @@
+src/main/obj/local/armeabi-v7a/objs/YARGI/Helper/plthook_elf.o: \
+  src/main/jni/Helper/plthook_elf.cpp \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/__config \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/string.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/errno.h \
+  src/main/jni/Helper/plthook.h src/main/jni/Helper/fake_dlfcn.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/cstdlib
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/__config:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+src/main/jni/Helper/plthook.h:
+
+src/main/jni/Helper/fake_dlfcn.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/cstdlib:
