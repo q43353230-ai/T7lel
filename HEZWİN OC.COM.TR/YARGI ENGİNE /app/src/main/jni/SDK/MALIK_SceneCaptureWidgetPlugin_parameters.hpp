4d26bb7a460c09367abcee9e6bd68195a7c000bc
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:17:52 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_SceneCaptureWidgetPlugin_parameters.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:33 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function SceneCaptureWidgetPlugin.SceneCaptureWidget.SetSceneCaptureCameraActor
+struct USceneCaptureWidget_SetSceneCaptureCameraActor_Params
+{
+	class ASceneCaptureCameraActor*                    InSceneCaptureCameraActor;                                // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+}
+
