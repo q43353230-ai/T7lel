749d066000752e10392864f2db82ab03c7f0f596
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:20:52 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_KantanChartsDatasource_structs.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:33 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct KantanChartsDatasource.KantanCartesianDatapoint
+// 0x0008
+struct FKantanCartesianDatapoint
+{
+	struct FVector2D                                   Coords;                                                   // 0x0000(0x0008) (Edit, BlueprintVisible, IsPlainOldData)
+};
+
+}
+
