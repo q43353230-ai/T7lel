57c6f4a5a030770d4eeae276d92ee7a3e19891e3
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:53:00 +0200

    Fast parallel upload - 2026-03-27 07:51:54.845063

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ScriptPlugin_structs.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:31 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct ScriptPlugin.SonInfo
+// 0x0010
+struct FSonInfo
+{
+	int                                                xlevel;                                                   // 0x0000(0x0004) (ZeroConstructor, IsPlainOldData)
+	struct FString                                     xname;                                                    // 0x0004(0x000C) (ZeroConstructor)
+};
+
+// ScriptStruct ScriptPlugin.PlayerInfo
+// 0x0024
+struct FPlayerInfo
+{
+	int                                                Level;                                                    // 0x0000(0x0004) (ZeroConstructor, IsPlainOldData)
+	struct FString                                     Name;                                                     // 0x0004(0x000C) (ZeroConstructor)
+	int                                                LocalPosition;                                            // 0x0010(0x0004) (ZeroConstructor, IsPlainOldData)
+	struct FSonInfo                                    mySon;                                                    // 0x0014(0x0010)
+};
+
+// ScriptStruct ScriptPlugin.LuaStateGuard
+// 0x0004
+struct FLuaStateGuard
+{
+	unsigned char                                      UnknownData00[0x4];                                       // 0x0000(0x0004) MISSED OFFSET
+};
+
+// ScriptStruct ScriptPlugin.LuaStateWrapperInitParams
+// 0x007C
+struct FLuaStateWrapperInitParams
+{
+	unsigned char                                      UnknownData00[0x7C];                                      // 0x0000(0x007C) MISSED OFFSET
+};
+
+}
+
