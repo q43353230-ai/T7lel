f4000f80fb23d8e65b10e716a3006563e3561c40
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:52:52 +0200

    Fast parallel upload - 2026-03-27 07:51:54.845063

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_QRCodeUtility_parameters.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function QRCodeUtility.ZXingScanner.Encode
+struct UZXingScanner_Encode_Params
+{
+	struct FString                                     Text;                                                     // (Parm, ZeroConstructor)
+	class UTexture2D*                                  ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+};
+
+// Function QRCodeUtility.ZXingScanner.Decode
+struct UZXingScanner_Decode_Params
+{
+	class UTexture2D*                                  Texture;                                                  // (Parm, ZeroConstructor, IsPlainOldData)
+	struct FZXingScanResult                            OutResult;                                                // (Parm, OutParm)
+	struct FVector4                                    InRect;                                                   // (ConstParm, Parm, OutParm, ReferenceParm, IsPlainOldData)
+	bool                                               ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+};
+
+}
+
