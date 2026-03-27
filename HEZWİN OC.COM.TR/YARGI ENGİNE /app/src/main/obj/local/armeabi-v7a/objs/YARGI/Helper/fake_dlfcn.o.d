cc2e3ce3e5e37f7cad8b1301c791eb2bb649a625
q43353230-ai <q43353230@gmail.com>
2026-03-27 08:16:55 +0200

    رك

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/obj/local/armeabi-v7a/objs/YARGI/Helper/fake_dlfcn.o.d
@@ -0,0 +1,14 @@
+src/main/obj/local/armeabi-v7a/objs/YARGI/Helper/fake_dlfcn.o: \
+  src/main/jni/Helper/fake_dlfcn.cpp \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/__config \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/string.h
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/__config:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/data/user/0/com.idragoncheats.studiopro/no_backup/ndksupport-1710240003/android-ndk-aide/sources/cxx-stl/llvm-libc++/include/string.h:
