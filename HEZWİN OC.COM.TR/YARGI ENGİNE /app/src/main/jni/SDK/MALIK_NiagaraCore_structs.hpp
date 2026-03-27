979597a29ca4e3e78442b5a54a94e31453edf3bb
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:37:24 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_NiagaraCore_structs.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct NiagaraCore.NiagaraCompileHash
+// 0x000C
+struct FNiagaraCompileHash
+{
+	TArray<unsigned char>                              DataHash;                                                 // 0x0000(0x000C) (ZeroConstructor)
+};
+
+}
+
