32c985e1086e80a6bb19f7f0c0a5ea9f0725453e
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:28 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ImgMedia_parameters.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function ImgMedia.ImgMediaSource.SetSequencePath
+struct UImgMediaSource_SetSequencePath_Params
+{
+	struct FString                                     Path;                                                     // (Parm, ZeroConstructor)
+};
+
+// Function ImgMedia.ImgMediaSource.GetSequencePath
+struct UImgMediaSource_GetSequencePath_Params
+{
+	struct FString                                     ReturnValue;                                              // (ConstParm, Parm, OutParm, ZeroConstructor, ReturnParm)
+};
+
+// Function ImgMedia.ImgMediaSource.GetProxies
+struct UImgMediaSource_GetProxies_Params
+{
+	TArray<struct FString>                             OutProxies;                                               // (Parm, OutParm, ZeroConstructor)
+};
+
+}
+
