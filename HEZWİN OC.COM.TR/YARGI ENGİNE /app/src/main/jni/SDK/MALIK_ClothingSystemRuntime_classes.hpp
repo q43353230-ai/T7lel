29baeeb1aa8628d08e4fe4ac9554d871b57d40a0
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:15:34 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ClothingSystemRuntime_classes.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Classes
+//---------------------------------------------------------------------------
+
+// Class ClothingSystemRuntime.ClothingAssetCustomData
+// 0x0004 (0x0020 - 0x001C)
+class UClothingAssetCustomData : public UObject
+{
+public:
+	unsigned char                                      UnknownData00[0x4];                                       // 0x001C(0x0004) MISSED OFFSET
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class ClothingSystemRuntime.ClothingAssetCustomData");
+		return pStaticClass;
+	}
+
+};
+
+
+// Class ClothingSystemRuntime.ClothingAsset
+// 0x00F8 (0x0130 - 0x0038)
+class UClothingAsset : public UClothingAssetBase
+{
+public:
+	class UPhysicsAsset*                               PhysicsAsset;                                             // 0x0038(0x0004) (Edit, ZeroConstructor, IsPlainOldData)
+	struct FClothConfig                                ClothConfig;                                              // 0x003C(0x00BC) (Edit)
+	TArray<struct FClothLODData>                       LODData;                                                  // 0x00F8(0x000C) (ZeroConstructor)
+	TArray<int>                                        LodMap;                                                   // 0x0104(0x000C) (ZeroConstructor)
+	TArray<struct FName>                               UsedBoneNames;                                            // 0x0110(0x000C) (ZeroConstructor)
+	TArray<int>                                        UsedBoneIndices;                                          // 0x011C(0x000C) (ZeroConstructor)
+	int                                                ReferenceBoneIndex;                                       // 0x0128(0x0004) (ZeroConstructor, IsPlainOldData)
+	class UClothingAssetCustomData*                    CustomData;                                               // 0x012C(0x0004) (ZeroConstructor, IsPlainOldData)
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class ClothingSystemRuntime.ClothingAsset");
+		return pStaticClass;
+	}
+
+};
+
+
+// Class ClothingSystemRuntime.ClothingSimulationFactoryNv
+// 0x0000 (0x0020 - 0x0020)
+class UClothingSimulationFactoryNv : public UClothingSimulationFactory
+{
+public:
+
+	static UClass* StaticClass()
+	{
+        static UClass *pStaticClass = 0;
+        if (!pStaticClass)
+            pStaticClass = UObject::FindClass("Class ClothingSystemRuntime.ClothingSimulationFactoryNv");
+		return pStaticClass;
+	}
+
+};
+
+
+}
+
