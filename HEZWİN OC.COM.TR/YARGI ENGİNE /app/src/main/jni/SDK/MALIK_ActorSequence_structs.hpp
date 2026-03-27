f039af5546706ff5cdf5863733c52c9c424ebba7
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:13:12 +0200

    Ultra fast upload - 2026-03-27 07:12:30.877347

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_ActorSequence_structs.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Enums
+//---------------------------------------------------------------------------
+
+// Enum ActorSequence.EActorSequenceObjectReferenceType
+enum class EActorSequenceObjectReferenceType : uint8_t
+{
+	EActorSequenceObjectReferenceType__ContextActor = 0,
+	EActorSequenceObjectReferenceType__ExternalActor = 1,
+	EActorSequenceObjectReferenceType__Component = 2,
+	EActorSequenceObjectReferenceType__EActorSequenceObjectReferenceType_MAX = 3
+};
+
+
+
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct ActorSequence.ActorSequenceObjectReference
+// 0x0020
+struct FActorSequenceObjectReference
+{
+	EActorSequenceObjectReferenceType                  Type;                                                     // 0x0000(0x0001) (ZeroConstructor, IsPlainOldData)
+	unsigned char                                      UnknownData00[0x3];                                       // 0x0001(0x0003) MISSED OFFSET
+	struct FGuid                                       actorid;                                                  // 0x0004(0x0010) (IsPlainOldData)
+	struct FString                                     PathToComponent;                                          // 0x0014(0x000C) (ZeroConstructor)
+};
+
+// ScriptStruct ActorSequence.ActorSequenceObjectReferences
+// 0x000C
+struct FActorSequenceObjectReferences
+{
+	TArray<struct FActorSequenceObjectReference>       Array;                                                    // 0x0000(0x000C) (ZeroConstructor)
+};
+
+// ScriptStruct ActorSequence.ActorSequenceObjectReferenceMap
+// 0x0018
+struct FActorSequenceObjectReferenceMap
+{
+	TArray<struct FGuid>                               BindingIds;                                               // 0x0000(0x000C) (ZeroConstructor)
+	TArray<struct FActorSequenceObjectReferences>      References;                                               // 0x000C(0x000C) (ZeroConstructor)
+};
+
+}
+
