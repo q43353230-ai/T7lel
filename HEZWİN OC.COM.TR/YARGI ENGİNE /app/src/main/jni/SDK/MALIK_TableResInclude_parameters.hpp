66afe162ae8ecb1dc38a280e9a3e1d91bda940f3
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:38:02 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_TableResInclude_parameters.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:31 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function TableResInclude.EvoBaseMapUIMarkTableMap.TraversTable
+struct UEvoBaseMapUIMarkTableMap_TraversTable_Params
+{
+	class UUAEDataTable*                               TableData;                                                // (Parm, ZeroConstructor, IsPlainOldData)
+	struct FName                                       Key;                                                      // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function TableResInclude.EvoBaseModTableTestTableMap.TraversTable
+struct UEvoBaseModTableTestTableMap_TraversTable_Params
+{
+	class UUAEDataTable*                               TableData;                                                // (Parm, ZeroConstructor, IsPlainOldData)
+	struct FName                                       Key;                                                      // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+}
+
