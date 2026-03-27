ced6635a72c76353f0997728ba24656e2312dcd1
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:15:48 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_CreativeLua_functions.cpp
@@ -0,0 +1,79 @@
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:34 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Functions
+//---------------------------------------------------------------------------
+
+// Function CreativeLua.CreativeBridgeLuaVM.UGCLuaError
+// (Final, Native, Public, Const)
+// Parameters:
+// int                            errCode                        (Parm, ZeroConstructor, IsPlainOldData)
+
+void UCreativeBridgeLuaVM::UGCLuaError(int errCode)
+{
+	static UFunction *pFunc = 0;
+	if (!pFunc)
+		pFunc  = UObject::FindObject<UFunction>("Function CreativeLua.CreativeBridgeLuaVM.UGCLuaError");
+
+	UCreativeBridgeLuaVM_UGCLuaError_Params params;
+	params.errCode = errCode;
+
+	auto flags = pFunc->FunctionFlags;
+	pFunc->FunctionFlags |= 0x400;
+
+	UObject *currentObj = (UObject *) this;
+	currentObj->ProcessEvent(pFunc, &params);
+
+	pFunc->FunctionFlags = flags;
+}
+
+
+// Function CreativeLua.CreativeBridgeLuaVM.RegisterSluaCallUgcluaEventHandler
+// (Final, Native, Public)
+
+void UCreativeBridgeLuaVM::RegisterSluaCallUgcluaEventHandler()
+{
+	static UFunction *pFunc = 0;
+	if (!pFunc)
+		pFunc  = UObject::FindObject<UFunction>("Function CreativeLua.CreativeBridgeLuaVM.RegisterSluaCallUgcluaEventHandler");
+
+	UCreativeBridgeLuaVM_RegisterSluaCallUgcluaEventHandler_Params params;
+
+	auto flags = pFunc->FunctionFlags;
+	pFunc->FunctionFlags |= 0x400;
+
+	UObject *currentObj = (UObject *) this;
+	currentObj->ProcessEvent(pFunc, &params);
+
+	pFunc->FunctionFlags = flags;
+}
+
+
+// Function CreativeLua.CreativeBridgeLuaVM.PostInit
+// (Native, Public, BlueprintCallable)
+
+void UCreativeBridgeLuaVM::PostInit()
+{
+	static UFunction *pFunc = 0;
+	if (!pFunc)
+		pFunc  = UObject::FindObject<UFunction>("Function CreativeLua.CreativeBridgeLuaVM.PostInit");
+
+	UCreativeBridgeLuaVM_PostInit_Params params;
+
+	auto flags = pFunc->FunctionFlags;
+	pFunc->FunctionFlags |= 0x400;
+
+	UObject *currentObj = (UObject *) this;
+	currentObj->ProcessEvent(pFunc, &params);
+
+	pFunc->FunctionFlags = flags;
+}
+
+
+}
+
