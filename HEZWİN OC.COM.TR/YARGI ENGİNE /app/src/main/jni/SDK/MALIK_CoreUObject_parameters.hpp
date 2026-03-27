e50d56f26d9c612a354b24ce58413b14a5c04e8d
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:19:52 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_CoreUObject_parameters.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:29 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function CoreUObject.Object.ExecuteUbergraph
+struct UObject_ExecuteUbergraph_Params
+{
+	int                                                EntryPoint;                                               // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+}
+
