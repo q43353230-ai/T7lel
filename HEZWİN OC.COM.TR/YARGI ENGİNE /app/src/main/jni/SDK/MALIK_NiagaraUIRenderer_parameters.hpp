e8f5534135810912abc3e4ee6d93dc59ee4d56ed
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:17:01 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_NiagaraUIRenderer_parameters.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function NiagaraUIRenderer.NiagaraSystemWidget.SetIsActivated
+struct UNiagaraSystemWidget_SetIsActivated_Params
+{
+	bool                                               bInIsActive;                                              // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function NiagaraUIRenderer.NiagaraSystemWidget.GetNiagaraComponent
+struct UNiagaraSystemWidget_GetNiagaraComponent_Params
+{
+	class UNiagaraUIComponent*                         ReturnValue;                                              // (ExportObject, Parm, OutParm, ZeroConstructor, ReturnParm, InstancedReference, IsPlainOldData)
+};
+
+// Function NiagaraUIRenderer.NiagaraSystemWidget.GetIsActivated
+struct UNiagaraSystemWidget_GetIsActivated_Params
+{
+	bool                                               ReturnValue;                                              // (Parm, OutParm, ZeroConstructor, ReturnParm, IsPlainOldData)
+};
+
+// Function NiagaraUIRenderer.NiagaraSystemWidget.DeactivateSystem
+struct UNiagaraSystemWidget_DeactivateSystem_Params
+{
+};
+
+// Function NiagaraUIRenderer.NiagaraSystemWidget.ActivateSystem
+struct UNiagaraSystemWidget_ActivateSystem_Params
+{
+	bool                                               Reset;                                                    // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+}
+
