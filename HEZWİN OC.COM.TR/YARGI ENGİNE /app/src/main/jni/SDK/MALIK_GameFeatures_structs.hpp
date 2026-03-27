652ba647d0a2e296b1de13464f3bafb1ef56a8f3
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:20:09 +0200

    Ultra fast upload - 2026-03-27 07:18:17.620662

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_GameFeatures_structs.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:31 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct GameFeatures.GameFeatureComponentEntry
+// 0x0058
+struct FGameFeatureComponentEntry
+{
+	unsigned char                                      UnknownData00[0x28];                                      // 0x0000(0x0028) UNKNOWN PROPERTY: SoftClassProperty GameFeatures.GameFeatureComponentEntry.ActorClass
+	unsigned char                                      UnknownData01[0x28];                                      // 0x0028(0x0028) UNKNOWN PROPERTY: SoftClassProperty GameFeatures.GameFeatureComponentEntry.ComponentClass
+	unsigned char                                      bClientComponent : 1;                                     // 0x0050(0x0001) (Edit)
+	unsigned char                                      bServerComponent : 1;                                     // 0x0050(0x0001) (Edit)
+	unsigned char                                      UnknownData02[0x7];                                       // 0x0051(0x0007) MISSED OFFSET
+};
+
+// ScriptStruct GameFeatures.GameFeaturePluginStateMachineProperties
+// 0x0048
+struct FGameFeaturePluginStateMachineProperties
+{
+	unsigned char                                      UnknownData00[0x44];                                      // 0x0000(0x0044) MISSED OFFSET
+	class UGameFeatureData*                            GameFeatureData;                                          // 0x0044(0x0004) (ZeroConstructor, Transient, IsPlainOldData)
+};
+
+}
+
