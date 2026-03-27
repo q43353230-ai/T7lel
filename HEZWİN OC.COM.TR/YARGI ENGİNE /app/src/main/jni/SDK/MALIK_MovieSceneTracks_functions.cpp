ca1807e1578fa5406f87af4fd3d9a40fbc09a18f
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:53:43 +0200

    Fast parallel upload - 2026-03-27 07:52:19.730394

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_MovieSceneTracks_functions.cpp
@@ -0,0 +1,37 @@
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Functions
+//---------------------------------------------------------------------------
+
+// Function MovieSceneTracks.MovieSceneTransformOrigin.BP_GetTransformOrigin
+// (Event, Protected, HasDefaults, BlueprintCallable, BlueprintEvent, BlueprintPure, Const)
+// Parameters:
+// struct FTransform              ReturnValue                    (Parm, OutParm, ReturnParm, IsPlainOldData)
+
+struct FTransform UMovieSceneTransformOrigin::BP_GetTransformOrigin()
+{
+	static UFunction *pFunc = 0;
+	if (!pFunc)
+		pFunc  = UObject::FindObject<UFunction>("Function MovieSceneTracks.MovieSceneTransformOrigin.BP_GetTransformOrigin");
+
+	UMovieSceneTransformOrigin_BP_GetTransformOrigin_Params params;
+
+	auto flags = pFunc->FunctionFlags;
+
+	UObject *currentObj = (UObject *) this;
+	currentObj->ProcessEvent(pFunc, &params);
+
+	pFunc->FunctionFlags = flags;
+
+	return params.ReturnValue;
+}
+
+
+}
+
