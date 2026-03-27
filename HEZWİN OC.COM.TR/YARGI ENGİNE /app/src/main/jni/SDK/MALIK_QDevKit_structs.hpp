290887c8d3a16a4025102e3bd4d57a020e6fe46f
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:17:41 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_QDevKit_structs.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:35 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Enums
+//---------------------------------------------------------------------------
+
+// Enum QDevKit.EQFirebaseRemoteConfigStatus
+enum class EQFirebaseRemoteConfigStatus : uint8_t
+{
+	EQFirebaseRemoteConfigStatus__kUnfetched = 0,
+	EQFirebaseRemoteConfigStatus__kFetchedSuccessfully = 1,
+	EQFirebaseRemoteConfigStatus__kFetchedFailed = 2,
+	EQFirebaseRemoteConfigStatus__EQFirebaseRemoteConfigStatus_MAX = 3
+};
+
+
+
+}
+
