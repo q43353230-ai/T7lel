b817ff364c6de57aa84ec2f627029ec0d2cf4ef5
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:44 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_MRMesh_structs.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct MRMesh.MRMeshConfiguration
+// 0x0001
+struct FMRMeshConfiguration
+{
+	unsigned char                                      UnknownData00[0x1];                                       // 0x0000(0x0001) MISSED OFFSET
+};
+
+}
+
