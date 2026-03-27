540141214baf9c97a81581f8be68d8672768f41b
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:33 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_JsonUtilities_structs.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct JsonUtilities.JsonObjectWrapper
+// 0x0014
+struct FJsonObjectWrapper
+{
+	struct FString                                     jsonString;                                               // 0x0000(0x000C) (Edit, ZeroConstructor)
+	unsigned char                                      UnknownData00[0x8];                                       // 0x000C(0x0008) MISSED OFFSET
+};
+
+}
+
