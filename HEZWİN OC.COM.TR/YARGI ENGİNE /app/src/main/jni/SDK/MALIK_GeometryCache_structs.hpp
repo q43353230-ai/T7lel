fd0aecc1aa4df12801ccbf1674bc2c7e7fcee58a
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:16:15 +0200

    Ultra fast upload - 2026-03-27 07:14:06.900849

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_GeometryCache_structs.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:32 2025
+ 
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Script Structs
+//---------------------------------------------------------------------------
+
+// ScriptStruct GeometryCache.GeometryCacheMeshData
+// 0x0040
+struct FGeometryCacheMeshData
+{
+	unsigned char                                      UnknownData00[0x40];                                      // 0x0000(0x0040) MISSED OFFSET
+};
+
+// ScriptStruct GeometryCache.TrackRenderData
+// 0x0050
+struct FTrackRenderData
+{
+	unsigned char                                      UnknownData00[0x50];                                      // 0x0000(0x0050) MISSED OFFSET
+};
+
+// ScriptStruct GeometryCache.GeometryCacheMeshBatchInfo
+// 0x000C
+struct FGeometryCacheMeshBatchInfo
+{
+	unsigned char                                      UnknownData00[0xC];                                       // 0x0000(0x000C) MISSED OFFSET
+};
+
+}
+
