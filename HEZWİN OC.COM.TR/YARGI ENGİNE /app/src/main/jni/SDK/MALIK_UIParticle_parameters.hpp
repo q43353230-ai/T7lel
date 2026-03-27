12448fc3f86adef943a1ce41b0e05514fe92d97b
q43353230-ai <q43353230@gmail.com>
2026-03-27 07:38:11 +0200

    Parallel upload - 2026-03-27 07:35:23.201027

---
+++ HEZWİN OC.COM.TR/YARGI ENGİNE /app/src/main/jni/SDK/MALIK_UIParticle_parameters.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+// PUBG MOBILE (3.8.0) TELEGRAM MALIK:-@K_Y_2  
+// GENERATE ON Wed May  7 14:35:33 2025
+ 
+#include "../SDK.hpp"
+
+namespace SDK
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function UIParticle.UIParticle.StopEmit
+struct UUIParticle_StopEmit_Params
+{
+};
+
+// Function UIParticle.UIParticle.Stop
+struct UUIParticle_Stop_Params
+{
+};
+
+// Function UIParticle.UIParticle.SetPlayParticle
+struct UUIParticle_SetPlayParticle_Params
+{
+	bool                                               InPlayParticle;                                           // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function UIParticle.UIParticle.Play
+struct UUIParticle_Play_Params
+{
+};
+
+// Function UIParticle.UIParticleEmitter.StopEmit
+struct UUIParticleEmitter_StopEmit_Params
+{
+};
+
+// Function UIParticle.UIParticleEmitter.Stop
+struct UUIParticleEmitter_Stop_Params
+{
+};
+
+// Function UIParticle.UIParticleEmitter.SetPlayParticle
+struct UUIParticleEmitter_SetPlayParticle_Params
+{
+	bool                                               InPlayParticle;                                           // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+// Function UIParticle.UIParticleEmitter.Play
+struct UUIParticleEmitter_Play_Params
+{
+};
+
+}
+
